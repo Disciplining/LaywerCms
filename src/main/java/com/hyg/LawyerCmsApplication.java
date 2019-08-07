c10a57d80ba69f6dc8dfab84301c10a96ed77bf1
liyanxing <416698547@qq.com>
2019-08-07 14:20:59 +0800

    创建pojo

--- src/main/java/com/hyg/LawyerCmsApplication.java
+++ src/main/java/com/hyg/LawyerCmsApplication.java
@@ -1,10 +1,9 @@
 package com.hyg;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.**.mapper")
+//@MapperScan("com.**.mapper")
 @SpringBootApplication
 public class LawyerCmsApplication
 {
