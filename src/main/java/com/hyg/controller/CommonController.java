48c8435534d7bfaf701baf4328a04987e7069f81
liyanxing <416698547@qq.com>
2019-08-07 16:04:25 +0800

    更新配置文件

--- src/main/java/com/hyg/controller/CommonController.java
+++ src/main/java/com/hyg/controller/CommonController.java
@@ -2,6 +2,7 @@ package com.hyg.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 通用控制器
@@ -10,12 +11,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CommonController
 {
 	/**
+	 *  测试
+	 * @return
+	 */
+	@GetMapping("/test")
+	@ResponseBody
+	public String test()
+	{
+		return "success run";
+	}
+
+	/**
 	 * 设置首页
 	 * @return
 	 */
 	@GetMapping("/")
 	public String index()
 	{
-		return "lyxtest/test.html";
+		return "";
 	}
 }
