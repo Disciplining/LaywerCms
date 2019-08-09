e6fb32ab48a88368d73da3d9a068c5b07d4935a5
liyanxing <416698547@qq.com>
2019-08-09 10:37:49 +0800

    获得团队文集

--- src/main/java/com/hyg/pojo/Article.java
+++ src/main/java/com/hyg/pojo/Article.java
@@ -2,6 +2,8 @@ package com.hyg.pojo;
 
 import lombok.Data;
 
+import java.sql.Date;
+
 /**
  * 团队文集表
  */
@@ -10,18 +12,18 @@ public class Article
 {
 	private Integer articleId;
 	private Integer type;
-	private Integer articleTitle;
-	private Integer author;
-	private Integer intro;
-	private Integer content;
-	private Integer editDate;
+	private String articleTitle;
+	private String author;
+	private String intro;
+	private String content;
+	private Date editDate;
 	private Integer count;
 	private String deleteFlag;
 
 	public static class ArticleType
 	{
-		public static final Integer COMPANY_LAW = 1; // 公司法律
-		public static final Integer LABOUR_LAW = 2; // 劳动法律
-		public static final Integer FORMAL_LAW = 3; // 形式法律
+		public static final int COMPANY_LAW = 1; // 公司法律
+		public static final int LABOUR_LAW = 2; // 劳动法律
+		public static final int FORMAL_LAW = 3; // 形式法律
 	}
 }
