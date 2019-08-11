fa8061c6ddce3ec04029eeff6600e4717fd0efcc
liyanxing <416698547@qq.com>
2019-08-11 20:25:05 +0800

    准备删除律师

--- src/main/java/com/hyg/mapper/LawyerMapper.java
+++ src/main/java/com/hyg/mapper/LawyerMapper.java
@@ -4,6 +4,7 @@ import com.hyg.pojo.Lawyer;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,4 +27,11 @@ public interface LawyerMapper
 	@Insert("insert into `t_lawyer` (`lawyerName`,`lawyerLevel`,`introduction`,`lawyerImg`,`deleteFlag`)" +
 				"values (#{lawyerName},#{lawyerLevel},#{introduction},#{lawyerImg},#{deleteFlag});")
 	void insertOneLawyer(Lawyer lawyer);
+
+	/**
+	 * 根据id删除一个律师 逻辑删除
+	 * @param lawyerId
+	 */
+	@Update(" update `t_lawyer` set `deleteFlag`='1' where `lawyerId`=#{lawyerId} ")
+	void deleteOneLawyerById(int lawyerId);
 }
