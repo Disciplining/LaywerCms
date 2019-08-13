606010f865c4c875f53e2ffeede0b03e9bce93a0
liyanxing <416698547@qq.com>
2019-08-13 15:27:09 +0800

    添加轮播图片接口

--- src/main/java/com/hyg/pojo/Carousel.java
+++ src/main/java/com/hyg/pojo/Carousel.java
@@ -1,5 +1,6 @@
 package com.hyg.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.sql.Timestamp;
@@ -13,5 +14,7 @@ public class Carousel
 	private Integer id;
 	private String bannerName;
 	private String imgPath;
+
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
 	private Timestamp editDate;
 }
