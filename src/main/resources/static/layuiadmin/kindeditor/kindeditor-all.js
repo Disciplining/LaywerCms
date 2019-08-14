564afda49f8448a7118cfc7ce2713c33ffd1f10d
刘金鹏 <1638786338@qq.com>
2019-08-14 11:42:03 +0800

    liujinpeng

--- src/main/resources/static/layuiadmin/kindeditor/kindeditor-all.js
+++ src/main/resources/static/layuiadmin/kindeditor/kindeditor-all.js
@@ -266,8 +266,8 @@ K.options = {
 		'justifyfull', 'insertorderedlist', 'insertunorderedlist', 'indent', 'outdent', 'subscript',
 		'superscript', 'clearhtml', 'quickformat', 'selectall', '|', 'fullscreen', '/',
 		'formatblock', 'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold',
-		'italic', 'underline', 'strikethrough', 'lineheight', 'removeformat', '|', 'image', 'multiimage',
-		'flash', 'media', 'insertfile', 'table', 'hr', 'emoticons', 'baidumap', 'pagebreak',
+		'italic', 'underline', 'strikethrough', 'lineheight', 'removeformat', '|', 'image',
+		 'table', 'hr', 'emoticons', 'baidumap', 'pagebreak',
 		'anchor', 'link', 'unlink', '|', 'about'
 	],
 	noDisableItems : ['source', 'fullscreen'],
