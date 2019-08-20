38f01c8678f87b7d9856f7c85af6c2b7ef5e3c73
liyanxing <416698547@qq.com>
2019-08-20 21:00:08 +0800

    发布的第一版

--- src/main/java/com/hyg/LawyerCmsApplication.java
+++ src/main/java/com/hyg/LawyerCmsApplication.java
@@ -3,6 +3,8 @@ package com.hyg;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -10,11 +12,22 @@ import org.springframework.web.filter.CorsFilter;
 
 @MapperScan("com.**.mapper")
 @SpringBootApplication
-public class LawyerCmsApplication
+public class LawyerCmsApplication extends SpringBootServletInitializer
 {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(LawyerCmsApplication.class, args);
+	}
+
+	/**
+	 * 发布项目
+	 * @param builder
+	 * @return
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+	{
+		return builder.sources(this.getClass());
 	}
 
 	/**
