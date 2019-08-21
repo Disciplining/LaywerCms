f251f0f7c8b54de11f1db47a488a3c9c98e07755
liyanxing <416698547@qq.com>
2019-08-21 16:07:18 +0800

    0.0.2版

--- src/main/java/com/hyg/mapper/ChargeTypeQuestionMapper.java
+++ src/main/java/com/hyg/mapper/ChargeTypeQuestionMapper.java
@@ -16,6 +16,7 @@ import java.util.List;
 @Repository("chargeTypeQuestionMapper")
 public interface ChargeTypeQuestionMapper
 {
+	/*增*/
 	/**
 	 * 插入一条数据
 	 * @param question
@@ -23,7 +24,29 @@ public interface ChargeTypeQuestionMapper
 	@Insert(" insert into `t_chargeTypeQuestion` (`chargeTypeId`,`question`,`answer`,`editDate`,`count`,`deleteFlag`) " +
 				" values (#{chargeTypeId},#{question},#{answer},#{editDate},#{count},#{deleteFlag}) ")
 	void insertOneChargeTypeQuestion(ChargeTypeQuestion question);
+	/*---------------------------------------------------------------------------------------------------------------*/
 
+	/*删*/
+	/**
+	 * 根据id删除一条数据
+	 * @param id
+	 */
+	@Update(" update `t_chargeTypeQuestion` set `deleteFlag`='1' where `id`=#{id} ")
+	void deleteOneQuestionById(int id);
+	/*---------------------------------------------------------------------------------------------------------------*/
+
+	/*改*/
+	/**
+	 * 编辑一个问答
+	 *  更新字段：question、answer、editDate
+	 * @param question
+	 */
+	@Update(" update `t_chargeTypeQuestion` set `question`=#{question},`answer`=#{answer},`editDate`=#{editDate} " +
+			" where `id`=#{id} and `deleteFlag`!='1' ")
+	void updateEditOneQuestion(ChargeTypeQuestion question);
+	/*---------------------------------------------------------------------------------------------------------------*/
+
+	/*查*/
 	/**
 	 * 获得所有的数据
 	 * @return
@@ -32,26 +55,11 @@ public interface ChargeTypeQuestionMapper
 	List<ChargeTypeQuestion> listAllQuestion();
 
 	/**
-	 * 根据id删除一条数据
-	 * @param id
-	 */
-	@Update(" update `t_chargeTypeQuestion` set `deleteFlag`='1' where `id`=#{id} ")
-	void deleteOneQuestionById(int id);
-
-	/**
 	 * 根据id获得一条数据
 	 * @param id
 	 * @return
 	 */
 	@Select(" select * from `t_chargeTypeQuestion` where `id`=#{id} and `deleteFlag`!='1' ")
 	ChargeTypeQuestion getOneQuestionById(int id);
-
-	/**
-	 * 编辑一个问答
-	 *  更新字段：question、answer、editDate
-	 * @param question
-	 */
-	@Update(" update `t_chargeTypeQuestion` set `question`=#{question},`answer`=#{answer},`editDate`=#{editDate} " +
-				" where `id`=#{id} and `deleteFlag`!='1' ")
-	void updateEditOneQuestion(ChargeTypeQuestion question);
+	/*------------------------------------------------------------------------*/
 }
